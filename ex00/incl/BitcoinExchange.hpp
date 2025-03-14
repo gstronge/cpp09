9500a6b52cb12ad8db31ff1967267b6211a71756
gstronge <graham.stronge@gmail.com>
2025-03-14 01:19:02 +0100

    ex00: working program, but class not orthodox canonoical form

--- ex00/incl/BitcoinExchange.hpp
+++ ex00/incl/BitcoinExchange.hpp
@@ -4,14 +4,17 @@
 #include <fstream>
 #include <map>
 
+#include <iomanip>
 
 
 class BitcoinExchange
 {
 private:
-	const std::string								_inputFile;
-	const std::string								_dataFile = "data.csv";
-	std::map<std::string, float>					_dataMap;
+	const std::string							_inputFile;
+	const std::string							_dataFile = "data.csv";
+	// std::map<std::string, float>				_dataMap;
+	std::map<int, float>						_dataMap;
+	std::string									_errorMsg;
 
 public:
 	BitcoinExchange(const std::string inputFile);
@@ -21,10 +24,12 @@ public:
 	void parseDatabase();
 	void parseInput();
 
-	float	processDate(const std::string date);
-	float	processValue(const std::string value);
+	float	processDate(int inputDate);
 
 	// BitcoinExchange&	operator=(BitcoinExchange& other);
 
+	bool	inputIsValid(const std::string buffer);
+	bool	dateIsValid(const std::string dateStr);
+	int		dateToInt(const std::string dateStr);
 	// void	printMap();
 };
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -16,12 +16,15 @@ void BitcoinExchange::parseDatabase() {
 
 	std::string		buffer;
 	size_t			pos;
-
+	
 	while (std::getline(file, buffer))
 	{
 		pos = buffer.find(",");
 		if (pos != std::string::npos && !buffer.empty() && buffer != "date,exchange_rate")
-			_dataMap[buffer.substr(0, pos)] = std::stof(buffer.substr(pos + 1));
+		{
+						
+			_dataMap[dateToInt(buffer)] = std::stof(buffer.substr(pos + 1));
+		}
 	}
 	file.close();
 }
@@ -38,40 +41,118 @@ void BitcoinExchange::parseInput() {
 	}
 
 	std::string		buffer;
-	size_t			pos;
 	float			ex_rate;
 	float			value;
 
 	while (std::getline(file, buffer))
 	{
-		pos = buffer.find_first_not_of("0123456789-");
-		if ((pos == std::string::npos || buffer.find(" | ") == std::string::npos) && !buffer.empty())
-			std::cout << "Error: bad input => " << buffer << "\n";
-		if (pos != std::string::npos && buffer != "date | value")
+		if (inputIsValid(buffer))
 		{
-			ex_rate = processDate(buffer.substr(0, pos));
-			value = processValue(buffer.substr(pos + 3));
-			if (ex_rate >= 0 && value >= 0)
-				std::cout << buffer.substr(0, pos) << " => " << value << " = " << ex_rate * value << "\n";
-		}		
+			ex_rate = processDate(dateToInt(buffer));
+			value = std::stof(buffer.substr(13));
+			if (buffer != "date | value")
+				std::cout << buffer.substr(0, 10) << " => " << value << " = "<< std::fixed << std::setprecision(2) << ex_rate * value << "\n";
+		}
+		else if (buffer != "date | value")
+			std::cout << _errorMsg << "\n";	
 	}
 	file.close();
 }
 
-float	BitcoinExchange::processDate(const std::string date) {
-	if (date.empty())
-		return -1;
+float	BitcoinExchange::processDate(int inputDate) {
+	auto	iter = _dataMap.begin();
+
+	if (iter->first > inputDate)
+		return (iter->second);
+
+	iter = _dataMap.find(inputDate);
+	if (iter != _dataMap.end())
+		return (iter->second);
+	
+	while (iter == _dataMap.end())
+	{
+		inputDate--;
+		iter = _dataMap.find(inputDate);
+		if (iter != _dataMap.end())
+			return (iter->second);
+	}
 	return 0;
 }
 
-float	BitcoinExchange::processValue(const std::string value) {
-	if (value.empty())
-		return -1;
-	return 0;
+
+bool	BitcoinExchange::inputIsValid(const std::string buffer) {
+	if (buffer.empty())
+	{
+		_errorMsg = "Error: bad input => LINE IS EMPTY";
+		return (false);
+	}
+	if (buffer.find(" | ") == std::string::npos || buffer.size() < 14)
+	{
+		_errorMsg = "Error: bad input => " + buffer;
+		return (false);
+	}
+
+	std::string		date_str = buffer.substr(0, 10);
+	std::string		num_str = buffer.substr(13);
+
+	if (!dateIsValid(date_str))
+	{
+		_errorMsg = "Error: bad input => " + buffer;
+		return (false);
+	}
+	if (std::stof(num_str) <= 0)
+	{
+		_errorMsg = "Error: not a positive number";
+		return (false);
+	}
+	if (std::stof(num_str) > 1000)
+	{
+		_errorMsg = "Error: too large a number";
+		return (false);
+	}
+	return (true);
+}
+
+bool	BitcoinExchange::dateIsValid(const std::string dateStr) {
+	int		year = std::stoi(dateStr.substr(0, 4));
+	int		month = std::stoi(dateStr.substr(5, 2));
+	int		day = std::stoi(dateStr.substr(8, 2));
+
+	if (year < 2009 || month < 1 || month > 12 || day < 1)
+
+	switch (month) {
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			if (day > 31)
+				return (false);
+			break;
+		case 4: case 6: case 9: case 11:
+			if (day > 30)
+				return (false);
+			break;
+		case 2:
+			if ((year == 2012 || year == 2016 || year == 2020 || year == 2024) && month > 29)
+				return (false);
+			if (!(year == 2012 || year == 2016 || year == 2020 || year == 2024) && month > 28)
+				return (false);
+	}
+
+	if (dateStr.find_first_not_of("0123456789-") != std::string::npos || dateStr[4] != '-' || dateStr[7] != '-')
+		return (false);
+	return (true);
+}
+
+int	BitcoinExchange::dateToInt(const std::string dateStr) {
+	int		date;
+
+	date = 10000 * std::stoi(dateStr.substr(0, 4));
+	date += 100 * std::stoi(dateStr.substr(5, 2));
+	date += std::stoi(dateStr.substr(8, 2));
+
+	return (date);
 }
 
 
-#include <iomanip>
+
 
 // void BitcoinExchange::printMap() {
 // 	auto	iter = _dataMap.begin();
